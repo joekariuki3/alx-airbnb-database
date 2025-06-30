3bae19fbe05d2408a1c4436eadb329f676ae6ed3
joekariuki3 <joekariuki3@gmail.com>
2025-06-30 22:49:24 +0300

    Fix aliasing for row_number in property bookings query for consistency

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -15,9 +15,9 @@ ORDER BY u.user_id;
 -- The final result is ordered by booking rank and row number.
 -- This allows for easy identification of the most booked properties and their respective ranks.
 SELECT p.property_id, p.name, COUNT(b.booking_id) AS total_bookings,
-       ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number,
+       ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS "row_number",
        RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM property p
 LEFT JOIN booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
-ORDER BY booking_rank, row_number;
+ORDER BY booking_rank, "row_number";
