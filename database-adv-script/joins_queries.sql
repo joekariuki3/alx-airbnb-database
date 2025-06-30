b809bf14365dac9099d38faf26b13d544b1abccd
joekariuki3 <joekariuki3@gmail.com>
2025-06-30 12:06:32 +0300

    Fix SQL query to include ORDER BY clause for property reviews

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,8 @@ ON booking.user_id = user.id;
 -- (LEFT JOIN selects all records from left table(table 1), and matching records from right table(table 2) if nothing matches in right table returns 0/NULL)
 SELECT * from property
 LEFT JOIN review
-ON property.property_id = review.property_id;
+ON property.property_id = review.property_id
+ORDER BY property.property_id;
 
 -- all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 -- (FULL OUTER JOIN selects all records from both tables, and fills in NULLs for missing matches on either side)
