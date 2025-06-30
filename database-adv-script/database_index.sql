d41edbaaa4ac60cd04f93a40d9d64b37c6f2b1e5
joekariuki3 <joekariuki3@gmail.com>
2025-06-30 23:01:02 +0300

    Add performance analysis before and after index creation in database_index.sql; update index performance images

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,6 @@
+-- before index creation
+EXPLAIN ANALYZE SELECT u.user_id, u.first_name, COUNT(b.booking_id) AS total_bookings FROM user u LEFT JOIN booking b ON u.user_id = b.user_id GROUP BY u.user_id, u.first_name ORDER BY u.user_id;
+
 -- most used columns on user table
 -- user_id -> as primary key index is created automatically
 -- first_name
@@ -10,3 +13,7 @@ CREATE INDEX idx_user_first_name ON user(first_name);
 -- property_id -> as a primary key index is created automatically
 -- property_name
 CREATE INDEX idx_property_name ON property(name)
+
+
+-- after index creation
+EXPLAIN ANALYZE SELECT u.user_id, u.first_name, COUNT(b.booking_id) AS total_bookings FROM user u LEFT JOIN booking b ON u.user_id = b.user_id GROUP BY u.user_id, u.first_name ORDER BY u.user_id;
